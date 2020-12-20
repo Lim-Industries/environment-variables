5455804289315948029c5f35f5fe8374d1349907
strategicfinger <andrew.c.lim@gmail.com>
2020-12-20 21:36:59 +0000

    DEV-1 Refactor code

--- src/EnvironmentVariables.php
+++ src/EnvironmentVariables.php
@@ -1,4 +1,4 @@
-<?php
+<?php declare(strict_types=1);
 /**
  * Sets environment variables from a .env file.
  * 
@@ -14,21 +14,52 @@ class EnvironmentVariables
      * Checks to see if the .env exists and parses it to set the 
      * evironment variables.
      * 
+     * @param [type] $file
      * @return void
      */
-    public function __construct() 
+    public function __construct($file) 
     {
-        if (file_exists('./.env')) {
-            $env = file_get_contents('./.env');
-            $variables = explode(PHP_EOL, $env);
-            $variables = array_filter($variables);
-        
-            foreach ($variables as $variable) {
-                $keyValue = explode('=', $variable);
-                putenv($keyValue[0] . "=" . trim($keyValue[1]));
-            }
+        $contents = $this->loadFile($file);
+        if ($contents) {
+            $this->assignVariables($contents);
         }
     } 
+
+    /**
+     * Undocumented function
+     *
+     * @param [type] $file
+     * @return void
+     */
+    public function loadFile($file)
+    {
+        try {
+            // run your code here
+            return file_get_contents($file);
+        }
+        catch (Exception $e) {
+            echo $e->getMessage();
+        }
+    }
+
+    /**
+     * Undocumented function
+     *
+     * @param [type] $contents
+     * @return void
+     */
+    public function assignVariables($contents)
+    {
+        $variables = explode(PHP_EOL, $contents);
+        $variables = array_filter($variables);
+        foreach ($variables as $variable) {
+            $keyValue = explode('=', $variable);
+            putenv($keyValue[0] . "=" . trim($keyValue[1]));
+        }
+    }
+
+    // asign vars
+    // load file
 }
 
 // new EnvironmentVariables();
