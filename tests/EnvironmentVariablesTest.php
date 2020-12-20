4a2c83c211af80d7f80ed60ec32796c5e468fc86
strategicfinger <andrew.c.lim@gmail.com>
2020-12-20 21:38:19 +0000

    DEV-1 Write unit tests

---
+++ tests/EnvironmentVariablesTest.php
@@ -0,0 +1,28 @@
+<?php declare(strict_types=1);
+use PHPUnit\Framework\TestCase;
+
+final class EnvironmentVariablesTest extends TestCase
+{
+    public function testCanUseEnvironmentVariables(): void
+    {
+        $simple = new EnvironmentVariables(__DIR__ . DIRECTORY_SEPARATOR . '.env');
+
+		$this->assertEquals('testing', getenv('TEST'));
+    }
+
+    public function testCanLoadFile(): void
+    {
+        $class = new EnvironmentVariables(__DIR__ . DIRECTORY_SEPARATOR . '.env');
+        $this->assertEquals(
+            file_get_contents(__DIR__ . DIRECTORY_SEPARATOR . '.env'), 
+            $class->loadFile(__DIR__ . DIRECTORY_SEPARATOR . '.env')
+        );
+    }
+
+    public function testCanAssignVariables(): void
+    {
+        $class = new EnvironmentVariables(__DIR__ . DIRECTORY_SEPARATOR . '.env');
+        $class->assignVariables('TEST2=testing2');
+        $this->assertEquals('testing2', getenv('TEST2'));
+    }
+}
